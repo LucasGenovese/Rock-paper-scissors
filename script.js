0bd0bc17e52c25e1cc2bf6fb1832582cc1a22ce4
Lucas Genovese Desktop <lucas59311@gmail.com>
2022-04-09 22:45:09 -0300

    Game logic and 5 round game

--- script.js
+++ script.js
@@ -16,7 +16,80 @@ function computerSelection (){
     }
     return computerChoice;
 }
-console.log(computerSelection());
 
+function playerSelection (userInput){
+    userInput = userInput.toLowerCase();
+    return userInput;
+}
 
-// let userInput = prompt("Input rock, paper or scissors");
+function playRound(finalPlayerSelection, finalComputerSelection){
+    if (finalPlayerSelection == "rock"){
+        if (finalComputerSelection == "rock"){
+            console.log("Tie");
+            return "tie";
+        }
+        else if (finalComputerSelection == "scissors"){
+            console.log("You win!");
+            return "win";
+        }
+        else {
+            console.log("You lose :(");
+            return "lose";
+        }
+    }
+
+    if (finalPlayerSelection == "paper"){
+        if (finalComputerSelection == "paper"){
+            console.log("Tie");
+            return "tie";
+        }
+        else if (finalComputerSelection == "rock"){
+            console.log("You win!");
+            return "win";
+        }
+        else {
+            console.log("You lose :(")
+            return "lose";
+        }
+    }
+
+    if (finalPlayerSelection == "scissors"){
+        if (finalComputerSelection == "scissors"){
+            console.log("Tie");
+            return "tie";
+        }
+        else if (finalComputerSelection == "paper") {
+            console.log("You win!");
+            return "win";
+        }
+        else {
+            console.log("You lose :(");
+            return "lose";
+        }
+    }
+}
+
+function game(){
+
+    let score = 0; 
+
+    for (let i = 0; i <5; i++){
+        userInput = prompt("Input rock, paper or scissors");
+        finalPlayerSelection = playerSelection(userInput);
+        finalComputerSelection = computerSelection();
+
+        checkWin = playRound(finalPlayerSelection, finalComputerSelection);
+        if (checkWin == "win"){
+            score = score + 1;
+        }
+    }
+
+    if (score >= 3){
+        console.log("Player wins!");
+    } else {
+        console.log("Computer wins :(");
+    }
+
+}
+
+game();
